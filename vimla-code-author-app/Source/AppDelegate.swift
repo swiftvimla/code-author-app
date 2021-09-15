cc36068533bea612c30178e9b4f7b6f92759b507
Jonatan Sundqvist <jonatan.sundqvist@vimla.se>
2021-09-15 15:27:53 +0200

    Is this the initial commit?

--- vimla-code-author-app/Source/AppDelegate.swift
+++ vimla-code-author-app/Source/AppDelegate.swift
@@ -9,80 +9,108 @@
 import Cocoa
 import SwiftUI
 import HotKey
+import UserNotifications
 
 func openProjectSearch(close: @escaping () -> Void) -> NSWindow {
-    let window = NSWindow(
-        contentRect: NSRect(x: 0, y: 0, width: 800, height: 500),
-        styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-        backing: .buffered, defer: false)
+  let window = NSWindow(
+      contentRect: NSRect(x: 0, y: 0, width: 800, height: 500),
+      styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+      backing: .buffered, defer: false)
   let contentView = SearchProjects(close: { window.close() }, within: UserDefaults.vimlaURL).environmentObject(UserDefaults.model)
   window.contentView = NSHostingView(rootView: contentView)
-    window.center()
-    window.isReleasedWhenClosed = false // TODO: App crashes on second call to this function (not during the call but after) without this line, but ideally we _should_ release the window when it's closed.
-    window.setFrameAutosaveName("Main Window")
-    window.makeKeyAndOrderFront(nil)
-    window.styleMask = .borderless
-    
-    return window
+  window.center()
+  window.isReleasedWhenClosed = false // TODO: App crashes on second call to this function (not during the call but after) without this line, but ideally we _should_ release the window when it's closed.
+  window.setFrameAutosaveName("Main Window")
+  window.makeKeyAndOrderFront(nil)
+  window.styleMask = .borderless
+
+  return window
 }
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+  var window: NSWindow?
+  var statusItem: NSStatusItem?
 
-    var window: NSWindow?
-    var statusItem: NSStatusItem?
+  let hotKey = HotKey(key: .e, modifiers: [.command, .shift])
+  
+  var searchWindow: NSWindow? = nil
+  
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Sandbox
+    
+    // Create the SwiftUI view that provides the window contents.
+    let contentView = ContentView()
+  
+    // Create the window and set the content view.
+    let window = NSWindow(
+        contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
+        styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+        backing: .buffered, defer: false)
+    window.center()
+    window.setFrameAutosaveName("Main Window")
+    window.contentView = NSHostingView(rootView: contentView)
+    window.makeKeyAndOrderFront(nil)
+    self.window = window
 
-    let hotKey = HotKey(key: .e, modifiers: [.command, .shift])
+    NSApplication.shared.isAutomaticCustomizeTouchBarMenuItemEnabled = true
+
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    statusItem.button?.title = "􀙤"
+    let menu = NSMenu()
+    statusItem.menu = menu
+    self.statusItem = statusItem
+
+    hotKey.keyDownHandler = {
+      print("Running hotkey handler")
+      statusItem.button?.title = "\(Int.random(in: 0 ... 10))"
+      if self.searchWindow != nil {
+        self.searchWindow?.close()
+        print("Done closing previous")
+      }
+      print("Gonna create new")
+      self.searchWindow = openProjectSearch(close: {})
+      print("Done creating new")
+    }
     
-    var searchWindow: NSWindow? = nil
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-      guard #available(macOS 11.0, *) else {
-          fatalError("Upgrade to macOS 11")
+    let center = UNUserNotificationCenter.current()
+    center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+      if let error = error {
+          // Handle the error here.
       }
       
-      let contentView = ContentView()
-      
-        // Create the window and set the content view.
-        let window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
-        self.window = window
-
-        NSApplication.shared.isAutomaticCustomizeTouchBarMenuItemEnabled = true
-
-        let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusItem.button?.title = "􀙤"
-        let menu = NSMenu()
-        statusItem.menu = menu
-        self.statusItem = statusItem
-
-        hotKey.keyDownHandler = {
-          print("Running hotkey handler")
-          statusItem.button?.title = "\(Int.random(in: 0 ... 10))"
-          if self.searchWindow != nil {
-            self.searchWindow?.close()
-            print("Done closing previous")
-          }
-          print("Gonna create new")
-          self.searchWindow = openProjectSearch(close: {})
-          print("Done creating new")
-        }
+      // Enable or disable features based on the authorization.
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-      print(#function)
+    
+    let content = UNMutableNotificationContent()
+    content.title = "Good morning, developer!"
+    content.body = "Brew some coffee and get on with it."
+    content.badge = 5
+  
+    var dateComponents = DateComponents()
+    dateComponents.calendar = Calendar.autoupdatingCurrent
+    dateComponents.weekday = dateComponents.day
+    dateComponents.hour = 23
+    dateComponents.minute = 59
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+    let uuidString = UUID().uuidString
+    let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+    let notificationCenter = UNUserNotificationCenter.current()
+    notificationCenter.add(request) { error in
+      if error != nil {
+        print(error)
+      }
     }
+  }
 
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-      return false
-    }
+  func applicationWillTerminate(_ aNotification: Notification) {
+      // Insert code here to tear down your application
+    print(#function)
+  }
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return false
+  }
 }
 
--- vimla-code-author-app/Source/Views/ContentView.swift
+++ vimla-code-author-app/Source/Views/ContentView.swift
@@ -11,16 +11,16 @@ import AppKit
 
 // Apparently required for the previews to work
 extension Character: ExpressibleByStringLiteral {
-    public init(stringLiteral value: StringLiteralType) {
-        self.init(value)
-    }
+  public init(stringLiteral value: StringLiteralType) {
+    self.init(value)
+  }
 }
 
 struct SearchProjectTextFieldStyle: TextFieldStyle {
-    func _body(configuration: TextField<Self._Label>) -> some View {
-        configuration
-            .font(.system(size: 42, weight: .heavy, design: .default))
-    }
+  func _body(configuration: TextField<Self._Label>) -> some View {
+    configuration
+      .font(.system(size: 42, weight: .heavy, design: .default))
+  }
 }
 
 struct QuitButton: View {
@@ -38,97 +38,153 @@ class Model: ObservableObject {
 
 @available(macOS 11.0, *)
 struct SearchProjects: View {
-    var close: () -> Void
-    @State var searchText: String = ""
-    @State var selected: Set<URL> = Set()
-    @EnvironmentObject var model: Model
-//    var projects: [Project]
-    
+  var close: () -> Void
+  @State var searchText: String = ""
+  @State var selected: Set<URL> = Set()
+  @EnvironmentObject var model: Model
+  //    var projects: [Project]
+  
   init(close: @escaping () -> Void, within directory: URL) {
-        self.close = close
+    self.close = close
+  }
+  
+  var searchResults: [Project] {
+    self.model.projects.filter { $0.label.contains(self.searchText) }
+  }
+  
+  var body: some View {
+    VStack {
+      AutofocusTextField(text: self.$searchText)
+        .font(.largeTitle.weight(.heavy))
+        .overlay(QuitButton(close: { self.close() }).padding(), alignment: Alignment(horizontal: .trailing, vertical: .center))
+        .padding()
+      List(self.searchResults, id: \.self, selection: self.$selected) { (project: Project) in
+        //                Image(systemSymbolName: "hammer", accessibilityDescription: nil)
+        Image(systemName: "hammer")
+        Button(
+          action: { openProject(at: project.root); self.close() },
+          label: { Text("OPEN") })
+        Button(
+          action: { openTerminal(at: project.root); self.close() },
+          label: { Text("TERMINAL") })
+        Text(project.activeBranch ?? "<no branch>")
+          .foregroundColor(.white)
+          .padding([.leading, .trailing], 8)
+          .padding([.top, .bottom], 3)
+          .fixedSize(horizontal: true, vertical: true)
+          .background(
+            RoundedRectangle(cornerRadius: 6)
+              .fill()
+              .foregroundColor(.blue)
+          )
+          .frame(minWidth: 100, alignment: .leading)
+        Text(project.label)
+          .font(.system(size: 18, weight: .light, design: .rounded))
+          .padding()
+      }
+      .focusable()
+      .listStyle(DefaultListStyle())
+      .frame(height: 220, alignment: .center)
     }
-    
-    var searchResults: [Project] {
-      self.model.projects.filter { $0.label.contains(self.searchText) }
-    }
-    
-    var body: some View {
-        VStack {
-            TextField(
-              "Search for project",
-              text: self.$searchText,
-              onEditingChanged: { _ in },
-              onCommit: { print("Done") }
-            )
-                .focusable()
-                .textFieldStyle(SearchProjectTextFieldStyle())
-                .overlay(QuitButton(close: { self.close() }).padding(), alignment: Alignment(horizontal: .trailing, vertical: .center))
-                .padding()
-            List(self.searchResults, id: \.self, selection: self.$selected) { (project: Project) in
-                Button(
-                    action: { openProject(at: project.root) },
-                    label: { Text("OPEN") })
-                Button(
-                    action: { openTerminal(at: project.root) },
-                    label: { Text("TERMINAL") })
-                Text(project.activeBranch ?? "<no branch>")
-                    .foregroundColor(.white)
-                    .padding([.leading, .trailing], 8)
-                    .padding([.top, .bottom], 3)
-                    .fixedSize(horizontal: true, vertical: true)
-                    .background(
-                        RoundedRectangle(cornerRadius: 6)
-                            .fill()
-                            .foregroundColor(.blue)
-                    )
-                    .frame(minWidth: 100, alignment: .leading)
-                Text(project.label)
-                    .font(.system(size: 18, weight: .light, design: .rounded))
-                    .padding()
-            }
-            .listStyle(DefaultListStyle())
-            .frame(height: 220, alignment: .center)
-        }
-        .frame(minWidth: 500)
-    }
+    .frame(minWidth: 500)
+  }
 }
 
 @available(macOS 11.0, *)
 struct ContentView: View, DropDelegate {
-    @State var text: String = ""
+  @State var text: String = ""
   
-    func performDrop(info: DropInfo) -> Bool {
-        print("Da, dropski boppsky")
-        print(info)
-        return true
-    }
+  func performDrop(info: DropInfo) -> Bool {
+    print("Da, dropski boppsky")
+    print(info)
+    return true
+  }
   
-    var body: some View {
-        HSplitView {
-            HStack {
-                Image(nsImage: #imageLiteral(resourceName: "heisenberg"))
-                    .resizable()
-                    .aspectRatio(contentMode: ContentMode.fit)
-                    .mask(Circle())
-                    .shadow(radius: 2)
-                Text("Werner von Heisenberg (developer)")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-            }.background(Color.green)
-            Text("Jonatan Sundqvist (developer)")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-            TextEditor(text: self.$text)
-        }.touchBar {
-            Button(action: { print("Hello World") }, label: { Text("Press") })
-        }
-        .onDrop(of: [.text], delegate: self)
+  var body: some View {
+    HSplitView {
+      HStack {
+        Image(nsImage: #imageLiteral(resourceName: "heisenberg"))
+          .resizable()
+          .aspectRatio(contentMode: ContentMode.fit)
+          .mask(Circle())
+          .shadow(radius: 2)
+        Text("Werner von Heisenberg (developer)")
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+      }.background(Color.green)
+      Text("Jonatan Sundqvist (developer)")
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+      TextEditor(text: self.$text)
+    }.touchBar {
+      Button(action: { print("Hello World") }, label: { Text("Press") })
     }
+    .onDrop(of: [.text], delegate: self)
+  }
+}
+
+class C: NSTextField {
+  init() {
+    super.init(frame: .zero)
+    
+    self.focusRingType = .none
+    self.isBordered = false
+    self.placeholderString = "Search for projects"
+    self.drawsBackground = false
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+struct AutofocusTextField: NSViewRepresentable {
+  static let font: NSFont? = NSFont(name: "Helvetica", size: 42)
+  @Binding var text: String
+  func makeNSView(context: NSViewRepresentableContext<AutofocusTextField>) -> NSTextField {
+    print("Creating new textfield")
+    var tf = C()
+    tf.delegate = context.coordinator
+    print(context.environment.font)
+    tf.font = AutofocusTextField.font
+    DispatchQueue.main.async {
+//      tf.font = context.environment.font
+      tf.becomeFirstResponder()
+    }
+    return tf
+  }
+
+  func updateNSView(_ nsView: NSTextField, context: NSViewRepresentableContext<AutofocusTextField>) {
+    print(#function)
+    nsView.stringValue = text
+    /// It seems as though the NSTextField is recreated on every update, and that some values are not preserved. Hence, we need to set the font on every update.
+    nsView.font = AutofocusTextField.font
+  }
+
+  func makeCoordinator() -> AutofocusTextField.Coordinator {
+    print(#function)
+    return Coordinator(parent: self)
+  }
+
+  class Coordinator: NSObject, NSTextFieldDelegate  {
+    let parent: AutofocusTextField
+    init(parent: AutofocusTextField) {
+      print(#function)
+      self.parent = parent
+    }
+
+    func controlTextDidChange(_ obj: Notification) {
+      print(#function)
+      let textField = obj.object as! NSTextField
+      textField.font = AutofocusTextField.font
+      parent.text = textField.stringValue
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-          ContentView()
-          SearchProjects(close: {}, within: UserDefaults.vimlaURL)
-        }
+  static var previews: some View {
+    Group {
+      ContentView()
+      SearchProjects(close: {}, within: UserDefaults.vimlaURL)
     }
+  }
 }
